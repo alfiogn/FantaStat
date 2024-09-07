d0fef304d36826d7afe4a3a4063bd056f7f41847
Negrini Giorgio, IT <giorgio.negrini@pirelli.com>
2024-09-07 16:25:23 +0200

    update main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from driver import Driver, FANTASTAT_PATH, HOME, DATA_PATH
-from player import Player
+from player import Player, TEAM_COLORS
 from scraper import Scraper
 
+import os
 import numpy as np
 import pandas as pd
 
+N_YEARS=1
+
 # Scrap and save data
-raw = Scraper(n_years=1)
+raw = Scraper(n_years=N_YEARS)
 raw.ScrapAll()
 # raw.UpdateAll()
 
